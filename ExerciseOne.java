47d57ebf36f4acf6ee8fcf8ddb9e338c9ca864b4
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2021-12-11 16:46:15 +0530

    Added a second method

--- ExerciseOne.java
+++ ExerciseOne.java
@@ -3,7 +3,8 @@ package DSA;
 public class ExerciseOne {
 
     public static void main(String[] args) {
-        numberOfOnes(14);
+//        numberOfOnes(14);
+        countNumberOfOnes(18);
     }
 
     public static void numberOfOnes(int decimalNumber) {
@@ -25,5 +26,48 @@ public class ExerciseOne {
         }
         System.out.println("Th binary equivalent of "+decimalNumber+" is "+ binaryEquivalent);
         System.out.println("Number of 1's in it are : "+countOfOnes);
+    }
+
+    public static void countNumberOfOnes(int num){
+
+        int temp = num; // a temporary variable to store the
+        // value of num
+
+        // to store the binary value
+        int[] binary = new int[20];
+
+        // to store the count of 1s
+        int count = 0;
+        int i;
+
+        // to iterate through the loop and keep a
+        // count of no.of digits in the obtained binary
+        for (i = 0; temp > 0; i++) {
+
+            // divide the number by 2
+            // and store the remainder
+            temp /= 2;
+            binary[i] = temp % 2;
+
+            // If 1 is present in binary
+            if (binary[i] == 1)
+
+                // increment the count if any digit
+                // is equal to 1
+                count++;
+        }
+
+        // Printing binary of decimal number
+        System.out.print("The binary equivalent of " + num
+                + " is : ");
+
+        // Iterating over array
+        for (int j = i - 1; j >= 0; j--)
+
+            // Printing obtained array in reverse order
+            System.out.print(binary[j]);
+
+        // Printing number of 1's
+        System.out.println("\nNumber of 1s is : " + count);
     }
 }
