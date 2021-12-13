e13d3c9aa1e18149d84a9985a8e85ca783852542
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2021-12-13 16:03:01 +0530

    Java program to reverse a number

--- ReverseOfANumber.java
+++ ReverseOfANumber.java
@@ -5,13 +5,15 @@ public class ReverseOfANumber {
     public static void main(String[] args) {
 
         int number = 12457823;
+        System.out.println("Number -> "+number);
         int reverse = 0;
 
         while (number!=0) {
             reverse = reverse*10+number % 10;
             number = number / 10;
         }
-        System.out.println(reverse);
+
+        System.out.println("Reverse of number -> "+reverse);
 
     }
 }
