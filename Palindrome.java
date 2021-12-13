66e01adeb3e9ffb83843b21426556dae61a4c157
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2021-12-13 17:23:24 +0530

    Java program to find factorial of a given number

---
+++ Palindrome.java
@@ -0,0 +1,26 @@
+package DSA;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        int input = 1221;
+        boolean itIs = isPalindrome(input);
+        if (itIs){
+            System.out.println(input+" is palindrome");
+        }
+        else {
+            System.out.println(input+" is not a palindrome");
+        }
+    }
+    static boolean isPalindrome(int num){
+        int number = num;
+        int reverse = 0;
+
+        while (number!=0) {
+            reverse = reverse*10+number % 10;
+            number = number / 10;
+        }
+
+        return reverse==num;
+    }
+
+}
