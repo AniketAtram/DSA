09b51f8a8d1e2e770271882379b262b58dd0e38c
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2021-12-13 16:16:54 +0530

    Java program to find fibonacci series upto the input number

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+package DSA;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        fibonacci(30);
+
+    }
+
+    static void fibonacci(int number){
+        int next, first=0, second=1;
+
+        while (first<=number){
+            System.out.print(first+" ");
+            next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
