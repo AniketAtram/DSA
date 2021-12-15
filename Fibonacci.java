63fcde5b42d697ec51d4f253e23640da5c08e311
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2021-12-15 19:58:09 +0530

    Java program to find fibonacci series

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,14 +3,14 @@ package DSA;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        fibonacci(30);
+        fibonacci(5);
 
     }
 
     static void fibonacci(int number){
         int next, first=0, second=1;
 
-        while (first<=number){
+        while (first<=number-1){
             System.out.print(first+" ");
             next = first + second;
             first = second;
