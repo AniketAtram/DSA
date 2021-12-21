069b8edeead075bef9dae6b5d3c735a0a1ce6a18
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2021-12-21 16:21:13 +0530

    Driver

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 package DSA;
 
 public class Main {
+    public static void main(String[] args) {
+        int len = 5;
 
-
+        for (int i=5-1;i<10;i++){
+            System.out.println(i);
+        }
+    }
 }
